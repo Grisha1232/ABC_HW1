1dba61c5cba3371a654b6477ef4a8e0411784e3c
Grisha1232 <58052549+Grisha1232@users.noreply.github.com>
2022-10-23 13:10:11 +0300

    Update main.s

--- Assembler non mod/main.s
+++ Assembler non mod/main.s
@@ -1,1 +1,89 @@
+	.file	"main.c"
+	.intel_syntax noprefix
+	.text
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"Enter the number of elements (0 < N <= %d): "
+.LC1:
+	.string	"%d"
+.LC2:
+	.string	"A[%d] = "
+.LC3:
+	.string	"%d "
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	push	rbp
+	mov	rbp, rsp
+	sub	rsp, 448
+	mov	DWORD PTR -436[rbp], edi  # argc
+	mov	QWORD PTR -448[rbp], rsi  # argv
+	mov	DWORD PTR -420[rbp], 0    # An
+	mov	DWORD PTR -8[rbp], 0      # Bn
+	mov	esi, 50                   # max_size
+	lea	rax, .LC0[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf@PLT
+	lea	rax, -420[rbp]
+	mov	rsi, rax
+	lea	rax, .LC1[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	__isoc99_scanf@PLT
+	mov	DWORD PTR -4[rbp], 0
+	jmp	.L2
+.L3:
+	mov	eax, DWORD PTR -4[rbp]
+	mov	esi, eax
+	lea	rax, .LC2[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf@PLT
+	lea	rdx, -208[rbp]
+	mov	eax, DWORD PTR -4[rbp]
+	cdqe
+	sal	rax, 2
+	add	rax, rdx
+	mov	rsi, rax
+	lea	rax, .LC1[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	__isoc99_scanf@PLT
+	add	DWORD PTR -4[rbp], 1
+.L2:
+	mov	eax, DWORD PTR -420[rbp]
+	cmp	DWORD PTR -4[rbp], eax
+	jl	.L3
+	mov	ecx, DWORD PTR -420[rbp]
+	lea	rdx, -416[rbp]
+	lea	rax, -208[rbp]
+	mov	esi, ecx
+	mov	rdi, rax
+	call	MakeB@PLT
+	mov	DWORD PTR -8[rbp], eax
+	mov	DWORD PTR -4[rbp], 0
+	jmp	.L4
+.L5:
+	mov	eax, DWORD PTR -4[rbp]
+	cdqe
+	mov	eax, DWORD PTR -416[rbp+rax*4]
+	mov	esi, eax
+	lea	rax, .LC3[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf@PLT
+	add	DWORD PTR -4[rbp], 1
+.L4:
+	mov	eax, DWORD PTR -4[rbp]
+	cmp	eax, DWORD PTR -8[rbp]
+	jl	.L5
+	mov	eax, 0
+	leave
+	ret
+	.size	main, .-main
+	.ident	"GCC: (Debian 11.3.0-5) 11.3.0"
+	.section	.note.GNU-stack,"",@progbits
 
